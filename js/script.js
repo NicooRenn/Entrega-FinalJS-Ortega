ac91209177e59c27bab3778060bb80648406d6de
NicooRenn <dnicolasortega@gmail.com>
2022-12-09 23:46:10 -0300

    Second Attempt

--- js/script.js
+++ js/script.js
@@ -1,69 +1,46 @@
-//declaracion de constantes
-$('#myModal').modal('show')
-/*
- const fechaNacimiento = 2009;
- let alertaEntrada = alert("Bienvenidos a Edicion Ilimitada! (por tu seguridad, en caso de comprar props debes ser mayor de 13 años)");
- let alertBox = true;
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+
+const display = new Display(displayValorAnterior, displayValorActual);
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value))
+});
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        1000
+    )
+});
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
 
 
-//declaracion de variables
+const fechaNacimiento = 2018;
+let alertaEntrada = alert("Estas a punto de presenciar a la calculadora mas innovadora de la historia");
+let alertBox = true;
+
 const accedio = localStorage.getItem("acceso-website")
 console.log(accedio)
 if (!accedio){
-    let acceso = parseInt(prompt("Por favor ingrese su año de Nacimiento."));
+    let acceso = parseInt(prompt("Por favor ingrese su año de Nacimiento, necesitas tener al menos 5 años para poder utilizar esta calculadora futurista."));
     while (alertBox) {console.log(acceso)
     if (acceso<=fechaNacimiento) {
         localStorage.setItem("acceso-website", acceso)
-        alert ("Si tienes alguna consulta o sugerencia, no dudes en hablarnos en la seccion Contacto!");
+        alert ("Welcome!:D");
         alertBox= false;
         }else { 
-        alert ("Lo sentimos :(, por tu seguridad necesitas si o si ser mayor de 13 para poder realizar una compra de una prop, cualquier consulta contactanos a nuestro telefono o instagram"); 
+        alert ("Lo sentimos :(, por tu seguridad necesitas si o si ser mayor de 5 para poder realizar un calculo aqui"); 
         break;
         }
-      }} */   
-// array con objeto
-const objetoCompra = [
-    {
-        id: 1,
-        imagen: "assets/img/HylianShield.png",
-        nombre: "Escudo Hyliano",
-        Precio: "$20000",
-        Tamaño: "51cm x 64cm",
-        Peso: "2.3 kg",
-        Descripcion: "Escudo del juego The Legend of Zelda: Skyward Sword"
-    },
-    {
-        id: 2,
-        imagen: "assets/img/ghostpokemon.png",
-        nombre: "Figura Pokemon Fantasmas",
-        Precio: "$25000",
-        Tamaño: "25cm x 25cm x 27cm",
-        Peso: "1 kg",
-        Descripcion: "Figura/Estatua de Resina de Pokemon tipo Fantasmas con luces"
-    },
-    {
-        id: 3,
-        imagen: "assets/img/thorhammer.png",
-        nombre: "Martillo de Thor",
-        Precio: "$10000",
-        Tamaño: "44cm x 21.9cm x 14cm",
-        Peso: "0.7 kg",
-        Descripcion: "Replica Martillo de Thor de las peliculas de Marvel Studios"
-    },
- ];
-
-//metodo de busqueda con find
-let valorBuscado = "Figura Pokemon Fantasmas"
-const busqueda = objetoCompra.find(objetoCompra => objetoCompra.nombre === valorBuscado)
-console.log(busqueda)
-
-const list = document.getElementById("itemlist");
-
-
-objetoCompra.forEach(elemento => {
-    list.innerHTML += `<div class="flexColumn col-12 col-lg-4">
-<img class="col-12 col-lg-12" src="${elemento.imagen}" alt="${elemento.Descripcion}">
-<h4>${elemento.nombre}</h4>
- <button class="primaryButton">Comprar</button> <br>
-</div>`
-});
+    }}   
